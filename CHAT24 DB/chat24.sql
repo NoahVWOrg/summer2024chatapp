63a50c596dfa77202b0014533c60b5d11430efc2
NoahVW <136670476+NoahVW@users.noreply.github.com>
2024-05-22 00:49:26 -0500

    Update chat24.sql

--- CHAT24 DB/chat24.sql
+++ CHAT24 DB/chat24.sql
@@ -4,44 +4,58 @@ CREATE DATABASE chat24;
 CREATE TABLE IF NOT EXISTS User (
 	userID INT PRIMARY KEY,
     username VARCHAR(15) UNIQUE,
-    password VARCHAR(255),
+    name VARCHAR(100),
     email VARCHAR(255) UNIQUE,
     DOB DATE,
+	password VARCHAR(255),
     publicKey TEXT,
     privateKeyHash TEXT
 );
 
 CREATE TABLE IF NOT EXISTS Chats (
 	chatID INT PRIMARY KEY,
-    userID INT, 
     creationDate DATE,
     
     FOREIGN KEY (userID) REFERENCES User(userID)
 );
 
 CREATE TABLE IF NOT EXISTS Messages (
-	chatID INT,
+	entryNum INT,
+    chatID INT,
     message TEXT,
     timestamp TIMESTAMP,
+    PRIMARY KEY(entryNum, chatID),
     
     FOREIGN KEY (chatID) REFERENCES Chats(chatID)
 );
 
-CREATE TABLE IF NOT EXISTS GroupInfo(
+CREATE TABLE IF NOT EXISTS GroupChat(
 	chatID INT,
-    memberID INT,
-    role VARCHAR(10),
+	memberID INT,
+    gName VARCHAR(255),
+    gAdmin VARCHAR(10),
     PRIMARY KEY(chatID, memberID),
     
 	FOREIGN KEY (chatID) REFERENCES Chats(chatID),
-	FOREIGN KEY (memberID) REFERENCES User(userID)
+	FOREIGN KEY (memberID) REFERENCES User(userID),
+    FOREIGN KEY (gAdmin) REFERENCES User(userID)
 );
 
-INSERT INTO User VALUES (1, 'vandeJ', 'pass1', 'vjake870@gmail.com', '2000-08-26', 'naofnowe k k', 'fuhihi9923jbj2o4');
-INSERT INTO User VALUES (2, 'vandeN ', 'pass3', 'vandenoa000@gmail.com', '2000-09-24', 'sniiasdf', 'v3v5vhj2hvhgjv2');
+CREATE TABLE IF NOT EXISTS DirectChat(
+	chatID INT,
+	memberID INT,
+    PRIMARY KEY(chatID, memberID),
+    
+	FOREIGN KEY (chatID) REFERENCES Chats(chatID),
+	FOREIGN KEY (memberID) REFERENCES User(userID),
+    FOREIGN KEY (gAdmin) REFERENCES User(userID)
+);
+
+INSERT INTO User VALUES (1, 'vandeJ', 'vjake870@gmail.com', '2000-08-26', 'pass1', 'naofnowe k k', 'fuhihi9923jbj2o4');
+INSERT INTO User VALUES (2, 'vandeN ', 'vandenoa000@gmail.com', '2000-09-24', 'pass3', 'sniiasdf', 'v3v5vhj2hvhgjv2');
 INSERT INTO Chats VALUES (1, 1, '2023-06-09');
-INSERT INTO groupinfo VALUES (1, 1, 'owner');  
-INSERT INTO groupinfo VALUES (1, 2, 'member');  
+INSERT INTO groupinfo VALUES (1, 1, 'Foodies',  'owner');  
+INSERT INTO groupinfo VALUES (1, 2, 'Foodies', 'member');  
 
 SELECT * FROM groupinfo;
 SELECT * FROM User;
